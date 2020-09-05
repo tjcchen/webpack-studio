b7c87be661bff667058ab0037e2ae1d5b5d39d09
Andy Chen <chenyangpoppin@hotmail.com>
2020-09-05 11:55:13 +0800

    Generate multiple bundle files with webpack config

--- my-project/src/search.js
+++ my-project/src/search.js
@@ -1,2 +1,1 @@
 document.write('<p>search.js: Webpack Search Page</p>');
-console.log('Webpack Search Page');
--- my-project/webpack.config.js
+++ my-project/webpack.config.js
@@ -3,25 +3,25 @@
 const path = require('path');
 
 // Single entry point file
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-};
-
-// Multiple output files
 // module.exports = {
 //   mode: 'production',
-//   entry: {
-//     index: './src/index.js',
-//     search: './src/search.js'
-//   },
+//   entry: './src/index.js',
 //   output: {
-//     path: path.join(__dirname, 'dist'),
-//     filename: '[name].js'  // a name placeholder is needed when output multiple files
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js'
 //   }
 // };
+
+// Multiple output files
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'  // a name placeholder is needed when output multiple files
+  }
+};
 
