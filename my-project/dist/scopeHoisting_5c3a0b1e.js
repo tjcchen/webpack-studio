8511f45eadcb645cfac970ff3e9d7320d6bd9827
Andy Chen <chenyangpoppin@hotmail.com>
2020-09-27 22:04:13 +0800

    chore: to test webpack import and export mechanism

--- my-project/dist/scopeHoisting_5c3a0b1e.js
+++ my-project/dist/scopeHoisting_5c3a0b1e.js
@@ -167,6 +167,12 @@ __webpack_require__.r(__webpack_exports__);
 document.write(Object(_scopeHoisting__WEBPACK_IMPORTED_MODULE_1__["scopeHoisting"])());
 console.log(Object(_common__WEBPACK_IMPORTED_MODULE_0__["common"])());
 
+var exportingFunc = function exportingFunc() {
+  return 'scope hoisting exporting funcion';
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (exportingFunc);
+
 /***/ }),
 
 /***/ 24:
--- my-project/src/scopeHoisting/index.js
+++ my-project/src/scopeHoisting/index.js
@@ -3,3 +3,9 @@ import { scopeHoisting } from './scopeHoisting';
 
 document.write(scopeHoisting());
 console.log(common());
+
+const exportingFunc = function() {
+  return 'scope hoisting exporting funcion';
+};
+
+export default exportingFunc;
