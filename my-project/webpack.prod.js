48141c9a4ba38014858170408eb2cfb0ebdae809
Andy Chen <chenyangpoppin@hotmail.com>
2020-09-29 12:06:08 +0800

    module: install npm eslint-loader module

--- my-project/webpack.prod.js
+++ my-project/webpack.prod.js
@@ -65,7 +65,9 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: [
+          'babel-loader'
+        ]
       },
       {
         test: /\.css$/,
