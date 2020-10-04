391dab2a67f59e04b4f7e9438d8a042d1f66520c
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-04 15:24:04 +0800

    chore: add some comments

--- large-number/webpack.config.js
+++ large-number/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     library: 'largeNumber',
-    libraryTarget: 'umd',
+    libraryTarget: 'umd',  // UMD - Universal Module Definition; support cjs, es6 module, amd simultaneously
     libraryExport: 'default'  // this is necessary, otherwise you need to write 'largeNumber.default' when you use
   },
   optimization: {
