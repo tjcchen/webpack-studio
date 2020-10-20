ef2739fc99e360718d0ba689d6b117a3e4da4f16
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-20 22:18:16 +0800

    feat: add smoke test code logic

--- builder-webpack/test/smoke/index.js
+++ builder-webpack/test/smoke/index.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const rimraf = require('rimraf');
+const Mocha = require('mocha');
+
+const mocha = new Mocha({
+  timeout: '10000ms'  // test timeout 10s
+});
 
 // Change root folder to 'test/smoke/template' with process.chdir()
 process.chdir(path.join(__dirname, 'template'));
@@ -14,10 +19,13 @@ rimraf('./dist', () => {
       process.exit(2);
     }
 
-    console.log(stats.toString({
-      colors: true,
-      modules: false,
-      children: false
-    }));
+    console.log(stats.toString({ colors: true, modules: false, children: false }));
+
+    console.log('Webpack build succeed, begin to run test.');
+
+    mocha.addFile(path.join(__dirname, 'html-test.js'));
+    mocha.addFile(path.join(__dirname, 'css-js-test.js'));
+
+    mocha.run();
   });
 });
