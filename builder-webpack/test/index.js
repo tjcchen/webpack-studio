78066fedba3de6f7c2f771b2c9b67cdf6fee05bf
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-22 09:26:10 +0800

    chore: rename thing

--- builder-webpack/test/index.js
+++ builder-webpack/test/index.js
@@ -5,5 +5,5 @@ process.chdir(path.join(__dirname, 'smoke/template'));
 
 describe('builder-webpack test case', () => {
   // ADD TEST CASES
-  require('./unit/webpack-base-test');
+  require('./unit/webpack-base.test');
 });
