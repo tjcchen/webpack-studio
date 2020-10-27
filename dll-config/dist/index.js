df61d48957200b62ce325833a288578ee4abcf8e
Andy Chen <chenyangpoppin@hotmail.com>
2020-10-27 12:03:23 +0800

    feat: add react code logic to render page

--- dll-config/dist/index.js
+++ dll-config/dist/index.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
@@ -16,10 +15,92 @@
   \**********************/
 /*! namespace exports */
 /*! exports [not provided] [no usage info] */
-/*! runtime requirements: __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n\n\n\nconsole.log('print something to the page!');\n\n//# sourceURL=webpack://dll-config/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\n\"use strict\";\n\nfunction _typeof(obj) { \"@babel/helpers - typeof\"; if (typeof Symbol === \"function\" && typeof Symbol.iterator === \"symbol\") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === \"function\" && obj.constructor === Symbol && obj !== Symbol.prototype ? \"symbol\" : typeof obj; }; } return _typeof(obj); }\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\nfunction _inherits(subClass, superClass) { if (typeof superClass !== \"function\" && superClass !== null) { throw new TypeError(\"Super expression must either be null or a function\"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }\n\nfunction _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }\n\nfunction _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }\n\nfunction _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === \"object\" || typeof call === \"function\")) { return call; } return _assertThisInitialized(self); }\n\nfunction _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError(\"this hasn't been initialised - super() hasn't been called\"); } return self; }\n\nfunction _isNativeReflectConstruct() { if (typeof Reflect === \"undefined\" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === \"function\") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }\n\nfunction _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }\n\n\n\n\n\nvar Content = /*#__PURE__*/function (_React$Component) {\n  _inherits(Content, _React$Component);\n\n  var _super = _createSuper(Content);\n\n  function Content() {\n    _classCallCheck(this, Content);\n\n    return _super.apply(this, arguments);\n  }\n\n  _createClass(Content, [{\n    key: \"render\",\n    value: function render() {\n      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n        \"class\": \"content\"\n      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"p\", null, \"Content Generated from React\"));\n    }\n  }]);\n\n  return Content;\n}(react__WEBPACK_IMPORTED_MODULE_0__.Component);\n\n;\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(Content, null), document.getElementById('root'));\n\n//# sourceURL=webpack://dll-config/./src/index.js?");
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!**************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference library ***!
+  \**************************************************************************/
+/*! dynamic exports */
+/*! export Children [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export Component [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export Fragment [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export Profiler [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export PureComponent [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export StrictMode [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export Suspense [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export __esModule [not provided] [no usage info] [missing usage info prevents renaming] */
+/*! export cloneElement [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export createContext [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export createElement [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export createFactory [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export createRef [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export forwardRef [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export isValidElement [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export lazy [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export memo [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useCallback [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useContext [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useDebugValue [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useEffect [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useImperativeHandle [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useLayoutEffect [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useMemo [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useReducer [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useRef [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export useState [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export version [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: module, __webpack_require__ */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = (__webpack_require__(/*! dll-reference library */ \"dll-reference library\"))(294);\n\n//# sourceURL=webpack://dll-config/delegated_./node_modules/react/index.js_from_dll-reference_library?");
+
+/***/ }),
+
+/***/ "./node_modules/react-dom/index.js":
+/*!******************************************************************************!*\
+  !*** delegated ./node_modules/react-dom/index.js from dll-reference library ***!
+  \******************************************************************************/
+/*! dynamic exports */
+/*! export __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export __esModule [not provided] [no usage info] [missing usage info prevents renaming] */
+/*! export createPortal [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export findDOMNode [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export flushSync [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export hydrate [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export render [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export unmountComponentAtNode [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export unstable_batchedUpdates [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export unstable_createPortal [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export unstable_renderSubtreeIntoContainer [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! export version [provided] [no usage info] [provision prevents renaming (no use info)] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: module, __webpack_require__ */
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = (__webpack_require__(/*! dll-reference library */ \"dll-reference library\"))(935);\n\n//# sourceURL=webpack://dll-config/delegated_./node_modules/react-dom/index.js_from_dll-reference_library?");
+
+/***/ }),
+
+/***/ "dll-reference library":
+/*!**************************!*\
+  !*** external "library" ***!
+  \**************************/
+/*! dynamic exports */
+/*! exports [maybe provided (runtime-defined)] [no usage info] */
+/*! runtime requirements: module */
+/***/ ((module) => {
+
+"use strict";
+eval("module.exports = library;\n\n//# sourceURL=webpack://dll-config/external_%22library%22?");
 
 /***/ })
 
--- dll-config/src/index.js
+++ dll-config/src/index.js
@@ -1,5 +1,17 @@
 "use strict";
 
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.less';
 
-console.log('print something to the page!');
+class Content extends React.Component {
+  render() {
+    return <div class="content">
+      <p>Content Generated from React</p>
+    </div>;
+  }
+};
+
+ReactDOM.render(<Content />, document.getElementById('root'));
